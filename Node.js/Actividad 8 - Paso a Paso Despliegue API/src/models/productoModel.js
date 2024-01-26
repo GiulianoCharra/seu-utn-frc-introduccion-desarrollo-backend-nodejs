8ca916ad9d0efc6d2ab71f40b6cdda3b9fd1d00f
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2024-01-26 17:17:50 -0300

    cambiado iniciado de la db

--- Node.js/Actividad 8 - Paso a Paso Despliegue API/src/models/productoModel.js
+++ Node.js/Actividad 8 - Paso a Paso Despliegue API/src/models/productoModel.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.MONGO_URL);
-runDB().catch((err) => console.log("El error es:", err));
+// Verificamos si la base de datos ya está conectada
+const isConnected = mongoose.connection.readyState;
+
+if (!isConnected) {
+  runDB().catch((err) => console.log("El error es:", err.reason));
+} else {
+  console.log("La base de datos ya está conectada");
+}
 
 async function runDB() {
   await mongoose.connect(process.env.MONGO_URL);
   console.log("Conectado a la base de datos");
 }
-
 const ProductoSchema = new mongoose.Schema(
   {
     nombre: String,
